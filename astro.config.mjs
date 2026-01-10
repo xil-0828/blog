01bb3491d0c6118c74b04bc1923f0a9545614dc3
XIL(しーる) <120433484+xil-0828@users.noreply.github.com>
2026-01-10 21:01:05 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://almondis.com',
+  site: 'https://almondis.com/',
   integrations: [mdx(), sitemap()],
 
   vite: {
