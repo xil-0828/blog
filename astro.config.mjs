fe0ee9be1424b58b367d18c4ff289de8cf6c4224
XIL(しーる) <120433484+xil-0828@users.noreply.github.com>
2026-01-13 05:41:40 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,17 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
+import partytown from '@astrojs/partytown';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://almondis.com/',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap() , partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),],
   build: {
     inlineStylesheets: 'always',
   },
